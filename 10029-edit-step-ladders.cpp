13f5ad9fcc4912b62423957198df7bfc11ed067e
danys <netfox2006@hotmail.com>
2014-10-06 22:48:03 +0200

    Efficient solution using hashing with chaining. Graph DFS

--- 10029-edit-step-ladders.cpp
+++ 10029-edit-step-ladders.cpp
@@ -6,6 +6,7 @@
 
 #define maxwords 25000
 #define alphalen 26
+
 #define primelen 100003
 
 using namespace std;
@@ -17,9 +18,9 @@ struct Node
 };
 
 Node graph[maxwords];
-int table[alphalen];
+int cache[maxwords];
 
-vector<vector<pair<int,string>>> hashtable(primelen);
+vector<vector<pair<int,string> > > hashtable(primelen);
 
 int hashcompute(string &s)
 {
@@ -27,50 +28,10 @@ int hashcompute(string &s)
     long long base = 1;
     for(unsigned int i=0;i<s.length();i++)
     {
-        res = (res + base*(int)(s[i]-'a')) % primelen;
+        res = (res + base*(int)(s[i])) % primelen;
         base = (base * alphalen) % primelen;
     }
     return (int)res;
-}
-
-bool match(string& str1, string& str2)
-{
-    int count=0;
-    for(unsigned int i=0;i<min(str1.length(),str2.length());i++) if (str1[i]!=str2[i]) count++;
-    return (count==1);
-}
-
-int matchunequal(string& str1, string& str2)
-{
-    memset(table,0,sizeof(int)*alphalen);
-    for(unsigned int i=0;i<min(str1.length(),str2.length());i++)
-    {
-        table[str1[i]-'a']+=1;
-        table[str2[i]-'a']-=1;
-    }
-    if (str1.length()>str2.length()) table[str1[str1.length()-1]-'a']+=1;
-    else table[str2[str2.length()-1]-'a']-=1;
-    int count=0;
-    int sum=0;
-    for(int i=0;i<alphalen;i++)
-    {
-        if (table[i]==0) count++;
-        sum += table[i];
-    }
-    if (count==alphalen-1) return sum;
-    else return 0;
-}
-
-bool editstep(string& str1, string& str2)
-{
-    if (str1.compare(str2)==0) return false;
-    int ldiff = str1.length()-str2.length();
-    if (abs(ldiff)>1) return false;
-    if (ldiff==0)
-    {
-        return match(str1,str2);
-    }
-    else return (matchunequal(str1,str2)==ldiff);
 }
 
 pair<int,string> findinhashtable(string &str)
@@ -83,7 +44,7 @@ pair<int,string> findinhashtable(string &str)
     return make_pair(-1,"");
 }
 
-void buildcandidates(string &str,vector<string> candidates)
+void buildcandidates(string &str,vector<string> &candidates)
 {
     candidates.clear();
     string word = str;
@@ -91,7 +52,7 @@ void buildcandidates(string &str,vector<string> candidates)
     //That char must be lexicographically following the char to be replaced.
     for(unsigned int i=0;i<word.length();i++)
     {
-        for(char c=word[i]+1;c<'z';c++)
+        for(char c=word[i]+1;c<='z';c++)
         {
             word[i]=c;
             candidates.push_back(word);
@@ -99,10 +60,10 @@ void buildcandidates(string &str,vector<string> candidates)
         word[i] = str[i];
     }
     //Compute all strings with one more char.
-    for(unsigned int i=0;i<word.length();i++)
+    for(unsigned int i=0;i<str.length();i++)
     {
         word.insert(word.begin()+i,'a');
-        for(char c=word[i]+1;c<'z';c++)
+        for(char c=str[i]+1;c<='z';c++)
         {
             word[i]=c;
             candidates.push_back(word);
@@ -129,24 +90,22 @@ void buildcandidates(string &str,vector<string> candidates)
 
 void buildgraph(int n)
 {
-    for(int i=0;i<n;i++)
+    vector<string> candidates;
+    pair<int,string> t;
+    for (int i = 0; i < n; i++)
     {
-        vector<string> candidates;
-        pair<int,string> t;
-        for (int i = 0; i < n; i++)
+        buildcandidates(graph[i].val, candidates);
+        for (unsigned int j = 0; j < candidates.size(); j++)
         {
-            buildcandidates(graph[i].val, candidates);
-            for (unsigned int j = 0; j < candidates.size(); j++)
-            {
-                t = findinhashtable(candidates[j]);
-                if (t.first != -1) graph[i].nodes.push_back(t.first);
-            }
+            t = findinhashtable(candidates[j]);
+            if (t.first != -1) graph[i].nodes.push_back(t.first);
         }
     }
 }
 
 int solve(int n,int startnode)
 {
+    if (cache[startnode]!=-1) return cache[startnode];
     if (graph[startnode].nodes.size()==0) return 1;
     int max=0;
     int cur;
@@ -155,6 +114,7 @@ int solve(int n,int startnode)
         cur = solve(n,graph[startnode].nodes[i]);
         if (cur>max) max = cur;
     }
+    cache[startnode]=1+max;
     return 1+max;
 }
 
@@ -180,6 +140,7 @@ int main()
 {
     string str;
     int n=0;
+    memset(cache,-1,maxwords*sizeof(maxwords));
     while(getline(cin,str))
     {
         graph[n++].val = str;
