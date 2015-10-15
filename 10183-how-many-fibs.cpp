75e78f37b9ceb3dbd2edcd8b7a6e26883f1c2f71
Dany <netfox2006@hotmail.com>
2015-10-15 23:27:07 +0200

    Removed gmp Bignum library dependency

--- 10183-how-many-fibs.cpp
+++ 10183-how-many-fibs.cpp
@@ -1,41 +1,131 @@
 #include <iostream>
-#include <gmp.h>
-#include <stdio.h>
 #include <vector>
-#include <string>
 
 #define maxFibs 1000
 
 using namespace std;
 
-mpz_t fibs[maxFibs];
+char* fibs[maxFibs];
+
+int len(char* x)
+{
+	int i=0;
+	while(x[i]!='\0') i++;
+	return i;
+}
+
+void printNum(char* x)
+{
+	int lenx = len(x);
+	for(int i=0;i<lenx;i++) cout << x[i];
+	cout << endl;
+}
+
+char* add(char* x, char* y)
+{
+	//cout << "Adding " << endl;
+	//printNum(x);
+	//printNum(y);
+	//cout << "OK" << endl;
+	//DEBUG
+	int lenx = len(x);
+	int leny = len(y);
+	int lenz = lenx>=leny ? lenx : leny;
+	int minxy = lenx<=leny ? lenx : leny;
+	//cout << "Lenx " << lenx << endl;
+	//cout << "Leny " << leny << endl;
+	//cout << "minxy " << minxy << endl;
+	lenz++;
+	//cout << "Lenz " << lenz << endl;
+	char* z = new char[lenz+1];//+1 for '\0' char
+	int digitx, digity, carry, digitz;
+	carry=0;
+	z[lenz]='\0';
+	for(int i=0;i<minxy;i++)
+	{
+		digitx = (int)(x[lenx-1-i]-'0');
+		digity = (int)(y[leny-1-i]-'0');
+		digitz = digitx+digity+carry;
+		carry = digitz / 10;
+		digitz %= 10;
+		//cout << "Digit " << (char)(digitz+'0') << endl;
+		z[lenz-1-i] = (char)(digitz+'0'); 
+	}
+	if (lenx>leny)
+	{
+		for(int i=lenx-1-minxy;i>=0;i--)
+		{
+			digitx = (int)(x[i]-'0');
+			digitz = carry+digitx;
+			carry = digitz / 10;
+			digitz %= 10;
+			//cout << "X> Digit " << (char)(digitz+'0') << endl;
+			z[i+lenz-lenx] = (char)(digitz+'0'); 
+		}
+	}
+	else if (lenx<leny)
+	{
+		for(int i=leny-1-minxy;i>=0;i--)
+		{
+			digity = (int)(y[i]-'0');
+			digitz = carry+digity;
+			carry = digitz / 10;
+			digitz %= 10;
+			//cout << "Y> Digit " << (char)(digitz+'0') << endl;
+			z[i+lenz-leny] = (char)(digitz+'0'); 
+		}
+	} //else if lenx==leny => do nothing
+	z[0]=(char)(carry+'0');
+	//Remove leading zeros
+	int first = 0;
+	while((first<lenz) && (z[first]=='0')){first++;}
+	if (first>0)
+	{
+		char* zz = new char[lenz+1-first];
+		for(int i=first;i<=lenz;i++) zz[i-first]=z[i];
+		z=zz;
+	}
+	//cout << "Add res " << endl;
+	//printNum(z);
+	//cout << "OK" << endl;
+	return z;
+}
+
+int cmp(char* x, char* y)
+{
+	int lenx = len(x);
+	int leny = len(y);
+	if (lenx>leny) return 1;
+	else if (lenx<leny) return -1;
+	int i=0;
+	while(x[i]==y[i]) i++;
+	if (x[i]>y[i]) return 1;
+	else if(x[i]<y[i]) return -1;
+	else return 0;
+}
 
 void compute(char* maxNum)
 {
-	mpz_t maxNumber;
-	mpz_init(maxNumber);
-	mpz_set_str(maxNumber,maxNum,10);
-	mpz_t fib1;
-	mpz_init(fib1);
-	mpz_set_si(fib1,1);
-	mpz_out_str(stdout, 10, fib1);
-  	putchar('\n');
-	mpz_t fib2;
-	mpz_init(fib2);
-	mpz_set_si(fib2,2);
-	mpz_set(fibs[0],fib1);
+	char* fib1;
+	char* fib2;
+	fib1 = new char[2];
+	fib2 = new char[2];
+	fib1[0] = '1';
+	fib1[1] = '\0';
+  	printNum(fib1);
+	fib2[0] = '2';
+	fib2[1] = '\0';
+	fibs[0] = fib1;
 	int counter=1;
-	mpz_t t;
-	mpz_init(t);
-	while(mpz_cmp(fib2,maxNumber)<=0)
+	char* t;
+	while(cmp(fib2,maxNum)<=0)
 	{
-		mpz_set(fibs[counter],fib2);
+		fibs[counter]=fib2;
 		counter++;
-		mpz_out_str(stdout, 10, fib2);
-  		putchar('\n');
-		mpz_add(t,fib1,fib2);
-		mpz_set(fib1,fib2);
-		mpz_set(fib2,t);
+		printNum(fib2);
+		t = add(fib1,fib2);
+		fib1 = fib2;
+		fib2 = t;
 	}
 	delete maxNum;
 	maxNum=0;
@@ -54,31 +144,31 @@ char* createMaxNumStr(int nDigits)
 	return str;
 }
 
-int findIndex(bool high, mpz_t x)
+int findIndex(bool high, char* x)
 {
 	int index=0;
-	while((index<maxFibs) && (mpz_cmp(fibs[index],x)<0)) index++;
+	while((index<maxFibs) && (cmp(fibs[index],x)<0)) index++;
 	if (high) return index;
 	else
 	{
-		if (mpz_cmp(fibs[index],x)==0) return index;
+		if (cmp(fibs[index],x)==0) return index;
 		else return --index;
 	}
 }
 
 int main()
 {
+	cout << ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>" << endl;
 	compute(createMaxNumStr(100));
-	mpz_t a,b;
-	mpz_init(a);
-	mpz_init(b);
+	/*char* a;
+	char* b;
 	string astr,bstr;
 	while(1)
 	{
 		cin >> astr >> bstr;
 		if ((astr.compare("0")==0) && (bstr.compare("0")==0)) break;
-		mpz_set_str(a,astr.c_str(),10);
-		mpz_set_str(b,bstr.c_str(),10);
+		a = (char*)astr.c_str();
+		b = (char*)bstr.c_str();
 		cout << findIndex(false,b)-findIndex(true,a)+1 << endl;
-	}
+	}*/
 }
