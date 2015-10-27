39efbc0d2d7cd1ea05718144c367ef7f7fbee971
danys <netfox2006@hotmail.com>
2015-10-27 23:03:20 +0100

    Adding BigNum addition, swap and compare functions. Trying subtraction.

--- 10213-how-many-pieces-of-land.cpp
+++ 10213-how-many-pieces-of-land.cpp
@@ -2,8 +2,15 @@
 
 using namespace std;
 
+int len(char* x)
+{
+	int i=0;
+	while(x[i]!='\0') i++;
+	return i;
+}
+
 //Convert a given integer into a char array
-char* convertToChar(int x)
+char* convertToChar(long long x)
 {
 	bool sign = true; //default to plus sign
 	if (x<0)
@@ -31,7 +38,7 @@ char* convertToChar(int x)
 }
 
 //Change the sign of the number x
-char* neg(char* x)
+char* neg(char* &x)
 {
 	//find current sign
 	if (x[0]=='-')	x[0]='+';
@@ -39,8 +46,238 @@ char* neg(char* x)
 	return x;
 }
 
+int compare(char* x, char* y, bool bypass)
+{
+	if (!bypass)
+	{
+		if ((x[0]=='-') && (y[0]=='+')) return -1;
+		else if ((x[0]=='+') && (y[0]=='-')) return 1;
+	}
+	int i=0;
+	int lenx = len(x);
+	int leny = len(y);
+	if (x[0]=='+')
+	{
+		if (lenx>leny) return 1;
+		else if (lenx<leny) return -1;
+	}
+	else
+	{
+		if (lenx>leny) return -1;
+		else if (lenx<leny) return 1;
+	}
+	while((x[i]==y[i]) && (x[i]!='\0')){i++;}
+	if (x[i]=='\0') return 0;
+	if (x[i]<y[i]) return -1;
+	else return 1;
+}
+
+int compareABS(char* x, char* y)
+{
+	return compare(x,y,true);
+}
+
+//Swap the given arguments
+void swap(char* &x, char* &y)
+{
+	char* t;
+	t = x;
+	x = y;
+	y = t;
+}
+
+void printNum(char* str)
+{
+	int i = 0;
+	while(str[i]!='\0')
+	{
+		cout << str[i];
+		i++;
+	}
+	cout << endl;
+}
+
+char* add(char* &x, char* &y)
+{
+	int xlen = len(x);
+	int ylen = len(y);
+	bool xsign = (x[0]=='+') ? true : false;
+	bool ysign = (y[0]=='+') ? true : false;
+	bool zsign = true;
+	if ((xsign) && (ysign))
+	{
+		//plus operation
+		zsign = true;
+	}
+	else if ((!xsign) && (!ysign))
+	{
+		//minus
+		x = neg(x);
+		y = neg(y);
+		zsign = false;
+	}
+	else if ((!xsign) && (ysign))
+	{
+		swap(x,y);
+		//now we have x with plus sign and y with minus sign
+		if (compareABS(x,y)>=0)
+		{
+			zsign = true;
+		}
+		else
+		{
+			zsign = false;
+			x = neg(x);
+			y = neg(y);
+			swap(x,y);
+		}
+	}
+	else if ((xsign) && (!ysign))
+	{
+		if (compareABS(x,y)>=0)
+		{
+			zsign = true;
+		}
+		else
+		{
+			zsign = false;
+			x = neg(x);
+			y = neg(y);
+			swap(x,y);
+		}
+	}
+	char* z;
+	char* zz;
+	int reslen = (xlen>=ylen) ? xlen : ylen;
+	int carry=0;
+	int minxy = (xlen<=ylen) ? xlen : ylen;
+	int digitx, digity, digitz;
+	if (y[0]=='+')
+	{
+		//plus operation
+		z = new char[reslen+2]; //null terminating char plus one more
+		z[reslen+1]='\0';
+		for(int i=0;i<minxy-1;i++)
+		{
+			digitx = x[xlen-1-i]-'0';
+			digity = y[ylen-1-i]-'0';
+			digitz = digitx + digity + carry;
+			carry = digitz / 10;
+			digitz %= 10;
+			z[reslen-i] = (char)(digitz+'0');
+		}
+		if (xlen>ylen)
+		{
+			for(int i=xlen-minxy;i>=1;i--)
+			{
+				digitx = x[i]-'0';
+				digitz = digitx + carry;
+				carry = digitz / 10;
+				digitz %= 10;
+				z[i+1] = (char)(digitz+'0');
+			}
+		}
+		else if (xlen<ylen)
+		{
+			for(int i=ylen-minxy;i>=1;i--)
+			{
+				digity = y[i]-'0';
+				digitz = digity + carry;
+				carry = digitz / 10;
+				digitz %= 10;
+				z[i+1] = (char)(digitz+'0');
+			}
+		}
+		if (carry>0)
+		{
+			z[1]=(char)(carry+'0');
+			z[0]=(zsign)?'+':'-';
+		}
+		else
+		{
+			zz = new char[reslen+1];
+			for(int i=1;i<reslen+1;i++) zz[i]=z[i+1];
+			z = zz;
+			z[0]=(zsign)?'+':'-';
+		}
+	}
+	else
+	{
+		//minus operation
+		z = new char[reslen+1]; //null terminating char plus one more
+		z[reslen]='\0';
+		for(int i=0;i<minxy-1;i++)
+		{
+			digitx = x[xlen-1-i]-'0';
+			digity = y[ylen-1-i]-'0';
+			if (digitx>=digity+carry)
+			{
+				digitz = digitx-digity-carry;
+				carry=0;
+			}
+			else
+			{
+				digitz = 10+digitx-digity-carry;
+				carry=1;
+			}
+			digitz %= 10;
+			z[reslen-i] = (char)(digitz+'0');
+		}
+		if (xlen>ylen)
+		{
+			for(int i=xlen-minxy;i>=1;i--)
+			{
+				digitx = x[i]-'0';
+				if (digitx>=carry)
+				{
+					digitz = digitx-carry;
+					carry=0;
+				}
+				else
+				{
+					digitz = 10+digitx-carry;
+					carry=1;
+				}
+				digitz %= 10;
+				z[i+1] = (char)(digitz+'0');
+			}
+		}
+		else if (xlen<ylen)
+		{
+			for(int i=ylen-minxy;i>=1;i--)
+			{
+				digity = y[i]-'0';
+				if (digity>=carry)
+				{
+					digitz = digity-carry;
+					carry=0;
+				}
+				else
+				{
+					digitz = 10+digity-carry;
+					carry=1;
+				}
+				digitz %= 10;
+				z[i+1] = (char)(digitz+'0');
+			}
+		}
+		if (z[1]==0)
+		{
+			int pos = 1;
+			while(z[pos]=='0') pos++;
+			zz = new char[reslen+1-pos];
+			for(int i=pos;i<reslen+1;i++) zz[i-pos+1]=z[i];
+			z = zz;
+			z[0]=(zsign)?'+':'-';
+		}
+		else z[0]=(zsign)?'+':'-';
+	}
+
+	return z;
+}
+
 //Computes f(x)=(x^4-6*x^3+23*x^2-18*x+24)/24
-long long compute(int x)
+/*long long compute(int x)
 {
 	if (x<=30000)
 	{
@@ -61,7 +298,7 @@ long long compute(int x)
 		res += (-18)*x;
 		char* cx = convertToChar(x);
 		char* cres = convertToChar(res);
-		char* c23x2 = mult(convertToChar(x2),convertToChar(6));
+		char* c23x2 = mult(convertToChar(x2),convertToChar(23));
 		cres = add(cres,c23x2);
 		char* cx3 = mult(convertToChar(x2),cx);
 		char* c6x3 = neg(mult(mult(convertToChar(x2),convertToChar(x)),convertToChar(6)));
@@ -70,16 +307,22 @@ long long compute(int x)
 		cres = add(cres,cx4);
 		return divideBy24(cres);
 	}
-}
+}*/
 
 int main(int argc, char** argv)
 {
-	int ncases,x;
+	char* x = convertToChar(-99928938);
+	char* y = convertToChar(9930490999);
+	char* z = add(x,y);
+	printNum(x);
+	printNum(y);
+	printNum(z);
+	/*int ncases,x;
 	cin >> ncases;
 	for(int z=1;z<=ncases;z++)
 	{
 		cin >> x;
 		cout << compute(x) << endl;
-	}
+	}*/
 	return 0;
 }
